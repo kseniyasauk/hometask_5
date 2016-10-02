879f69f5da95a6e88ee4e178ef40f208b11a6da2
kseniyasauk <kseniya_sauk@epam.com>
2016-10-02 13:49:53 +0300

    add task5_3_LINQtoXML

---
+++ Task5_3_LINQtoXML/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task5_3_LINQtoXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkWithCustomersXml xml = new WorkWithCustomersXml();
+            xml.Start();
+        }
+    }
+}
---
+++ Task5_3_LINQtoXML/WorkWithCustomersXml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task5_3_LINQtoXML
+{
+    class WorkWithCustomersXml
+    {
+        public static XDocument LoadXml()
+        {
+            return XDocument.Load(@"D:\Task5\Task5_3_LINQtoXML\Customers.xml");
+        }
+
+        public  Dictionary<string,double> CustomersWithSumOfOrder(XDocument xd)
+        {
+            Dictionary<string, double> dictionary = new Dictionary<string, double>();
+
+            var customers = xd.Elements()
+                .First()
+                .Elements();
+            foreach (var customer in customers)
+            {
+                double totalSum = 0;
+
+                var orders = customer
+                    .Elements()
+                    .Last()
+                    .Elements();
+
+                foreach (var order in orders)
+                {
+                    string value = order
+                        .Elements()
+                        .Last()
+                        .Value;
+                    string replace = value.Replace(".", ",");
+                    totalSum += double.Parse(replace);
+                }
+                string id = customer
+                    .Elements()
+                    .First()
+                    .Value;
+
+
+                dictionary.Add(id,totalSum);
+                
+            }
+
+            return dictionary;
+        }
+
+        public void Start()
+        {
+            var orders = CustomersWithSumOfOrder(LoadXml());
+            var bigOrders = orders.Where(el => el.Value > 1000.0).ToList();
+            foreach (var bigOrder in bigOrders)
+            {
+                Console.WriteLine(bigOrder);
+            }
+
+            Console.WriteLine($"Total: {bigOrders.Count}");
+
+            bigOrders = orders.Where(el => el.Value > 5000.0).ToList();
+            foreach (var bigOrder in bigOrders)
+            {
+                Console.WriteLine(bigOrder);
+            }
+
+            Console.WriteLine($"Total: {bigOrders.Count}");
+
+            bigOrders = orders.Where(el => el.Value > 3000.0).ToList();
+            foreach (var bigOrder in bigOrders)
+            {
+                Console.WriteLine(bigOrder);
+            }
+
+            Console.WriteLine($"Total: {bigOrders.Count}");
+
+            bigOrders = orders.Where(el => el.Value > 10000.0).ToList();
+            foreach (var bigOrder in bigOrders)
+            {
+                Console.WriteLine(bigOrder);
+            }
+
+            Console.WriteLine($"Total: {bigOrders.Count}");
+
+            bigOrders = orders.Where(el => el.Value > 20000.0).ToList();
+            foreach (var bigOrder in bigOrders)
+            {
+                Console.WriteLine(bigOrder);
+            }
+
+            Console.WriteLine($"Total: {bigOrders.Count}");
+        }
+    }
+}
